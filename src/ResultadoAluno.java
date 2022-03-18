38aa0c870f978705a0a948ace34d819860275b18
Isabella Jorge Ferreira <rm79580@estudante.fieb.edu.br>
2022-03-18 12:16:52 -0300

    Primeiro Commit

---
+++ src/ResultadoAluno.java
@@ -0,0 +1,20 @@
+import javax.swing.JOptionPane;
+
+public class ResultadoAluno {
+
+	public static void main(String[] args) {
+
+        double media = Double.parseDouble(JOptionPane.showInputDialog(null, "Media: "));
+        
+        
+        if(media >= 6.0) {
+        	JOptionPane.showMessageDialog(null,
+					"Aprovado");
+        } else if ( media < 6.0) {
+			JOptionPane.showMessageDialog(null,
+					"Reprovado");
+
+	}
+
+}
+}
